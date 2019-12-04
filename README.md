21b7873ecba08721fcad1a7f419493fcce011bb8
funlyonions <admin@example.com>
2019-12-04 11:51:36 -0500

    Created the readme and description

---
+++ README.md
@@ -0,0 +1,26 @@
+# Manifold: Effectively Create Powerful Rewards For Reinforcement Learning
+
+![logo](assets/logo.png)
+
+
+---
+
+`Manifold` is an open source Python framework for generating, experimenting, and deploying robust reward algorithms for reinforcement learning. The framework focuses on being highly composable and extensible, to create complex strategies that work on a single agent on one machine, to allow for creating multiple reward strategies for entire fleets of algorithms.
+
+We do this by focusing on using two major topics from complex geometry, optimization and dynamical systems: 
+
+1. `Manifolds` - Defined complex geometric spaces that resemble euclidian spaces. We use this to create a map of where we want to let the RL agent evolve.
+2. `Attractors` - A set of numerical values toward which a system tends to evolve, for a wide variety of starting conditions of the system.
+3. `Multi-Objective Scalarization` - A way to turn a `multi-objective` problems into a single number. This single number will allow us to feed information into the RL algorithm to optimize to. We can set rules of how scalarization will play out over time.
+
+
+## Design Philosophy
+
+Comment about keras design philosophy ... Need to go into details. 
+
+
+## Installation Instructions
+
+```
+pip install manifold-ml
+```
---
+++ setup.py
@@ -0,0 +1,65 @@
+import os
+import codecs
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+class UploadCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+        self.status("Building Source distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("sudo twine upload dist/*")
+        sys.exit()
+
+
+
+setup(
+    name="manifold-ml",
+    version="0.0.1",
+    author="Kevin Hill",
+    author_email="kah.kevin.hill@gmail.com",
+    description="Create Powerful Rewards For Reinforcement Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["manifold"],
+    install_requires=[
+        "redis", 'scipy', 'numpy'
+    ], 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={"upload": UploadCommand},
+    
+)
